cd378002bc6dddda2c5845106ff02ca3273ec726
anastasiia-kazmirchuk <anastasia_poetry@protonmail.com>
2021-08-25 19:25:01 +0300

    Initial commit

---
+++ src/tasks/basic_programm/you_cant_code_under_pressure_number_1.java
@@ -0,0 +1,17 @@
+package tasks.basic_programm;
+
+public class you_cant_code_under_pressure_number_1 {
+    public static void main(String[] args) {
+        // перевірка задачі
+        System.out.println(Java.doubleInteger(2));
+
+    }
+
+
+   static class Java {
+        public static int doubleInteger(int i) {
+            // Double the integer and return it!
+            return i*2;
+        }
+    }
+}
