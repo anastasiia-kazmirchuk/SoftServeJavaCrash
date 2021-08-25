efca8412c23100044b1ed3814e2c75042fa045f8
anastasiia-kazmirchuk <anastasia_poetry@protonmail.com>
2021-08-25 19:34:50 +0300

    Master

---
+++ src/tasks/basic_programm/function_3multiplying_two_numbers.java
@@ -0,0 +1,15 @@
+package tasks.basic_programm;
+
+public class function_3multiplying_two_numbers {
+    public static void main(String[] args) {
+        System.out.println(Kata.multiply(2,3));
+    }
+
+    public class Kata {
+        public static int multiply(int num1, int num2) {
+            // your code goes here
+
+            return num1*num2;
+        }
+    }
+}
